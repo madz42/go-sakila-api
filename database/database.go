c075e2f3b5d24bd35289556fb14386c60e02fc09
madz42 <fv.sqn.mad@gmail.com>
2023-03-08 23:21:50 +0100

    certificate and env

--- database/database.go
+++ database/database.go
@@ -1,6 +1,37 @@
+// package database
+
+// import (
+// 	"gorm.io/driver/mysql"
+// 	"gorm.io/gorm"
+// )
+
+// var DB *gorm.DB
+
+// func Init() {
+// 	conn := "root:MyN3wP4ssw0rd@tcp(localhost:3306)/sakila"
+
+// 	db, err := gorm.Open(
+// 		mysql.Open(conn+"?parseTime=true"),
+// 		&gorm.Config{},
+// 	)
+
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	DB = db
+// }
+
 package database
 
 import (
+	"crypto/tls"
+	"crypto/x509"
+	_ "database/sql"
+	"io/ioutil"
+	"os"
+
+	mysql_driver "github.com/go-sql-driver/mysql"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -8,10 +39,30 @@ import (
 var DB *gorm.DB
 
 func Init() {
-	conn := "root:MyN3wP4ssw0rd@tcp(localhost:3306)/sakila"
+	user := os.Getenv("DB_USER")
+	if len(user) == 0 {
+		user = "root"
+	}
+	password := os.Getenv("DB_PASSWORD")
+	address := os.Getenv("DB_ADDRESS")
+	if len(address) == 0 {
+		address = "localhost"
+	}
+
+	param_string := "?parseTime=true"
+	cert_path := os.Getenv("DB_TLS_CERT")
+	if len(cert_path) > 0 {
+		rootCertPool := x509.NewCertPool()
+		pem, _ := ioutil.ReadFile(cert_path)
+		rootCertPool.AppendCertsFromPEM(pem)
+		mysql_driver.RegisterTLSConfig("custom", &tls.Config{RootCAs: rootCertPool})
+		param_string += "&allowNativePasswords=true&tls=custom"
+	}
+
+	conn := (user + ":" + password + "@tcp(" + address + ":3306)/sakila" + param_string)
 
 	db, err := gorm.Open(
-		mysql.Open(conn+"?parseTime=true"),
+		mysql.Open(conn),
 		&gorm.Config{},
 	)
 
